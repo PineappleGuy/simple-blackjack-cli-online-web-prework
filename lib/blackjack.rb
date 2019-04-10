4eb4043ba9f5f1ef29331bc8f2ffb97128370bee
Garth Lilly <garff247@gmail.com>
2019-04-10 05:30:00 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -45,10 +45,10 @@ def hit?(num)
   # code hit? here
   prompt_user
   get_user_input
-  if get_user_input == "h"
+  if "h"
     deal_card
 
-  elsif get_user_input == "s"
+  elsif "s"
 
   else
     invalid_command
